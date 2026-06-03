735f5cb862632559916c4f500110c85b81fe77f1
hyun7931 <119102748+hyun7931@users.noreply.github.com>
2026-06-04 02:20:40 +0900

    feat(saga): SagaTransaction / SagaStepHistory Entity + Repository

---
+++ src/main/java/com/transaction/domain/saga/repository/SagaTransactionRepository.java
@@ -0,0 +1,10 @@
+package com.transaction.domain.saga.repository;
+
+import com.transaction.domain.saga.entity.SagaTransaction;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SagaTransactionRepository extends JpaRepository<SagaTransaction, Long> {
+
+    Optional<SagaTransaction> findByTransactionKey(String transactionKey);
+}
